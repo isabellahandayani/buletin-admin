0ec8803f3f0b44c901d4e2b8377e71d49af0dc1d
isabellahandayani <isabellahandayani@gmail.com>
2022-02-21 17:04:11 +0700

    fix: role route problem

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -1,8 +1,16 @@
+import { Center, Heading, Text, Flex } from "@chakra-ui/react";
 
 const NotFound = () => {
   return (
-	<div>NotFound</div>
-  )
-}
+    <Center mt={300}>
+      <Flex flexDirection="column">
+        <Center>
+          <Heading size="4xl">404</Heading>
+        </Center>
+        <Text size="lg">Page Not Found</Text>
+      </Flex>
+    </Center>
+  );
+};
 
-export default NotFound
+export default NotFound;
